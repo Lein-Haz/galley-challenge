c548665da58676c6fe9f537fbc65b302f54223e3
P <paz5x4@mail.umsl.edu>
2017-11-09 18:00:42 -0800

    Brought in material, added missing material req, updated spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {AppMaterialModule} from "./app-material/app-material.module";
+import {By} from "@angular/platform-browser";
+import {MatToolbar} from "@angular/material";
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[
+        AppMaterialModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the OTP app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
+  it('should have a toolbar component', () => {
+    fixture.detectChanges();//It does something weird without this line
+    let sideNav = fixture.debugElement.query(By.directive(MatToolbar));
+    expect(sideNav).not.toBe(null);
+  });
   it(`should have as title 'OTP'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
