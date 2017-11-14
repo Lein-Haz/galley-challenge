a0688dc3a261aa64d1cfa652a012e26fe90277c7
P <paz5x4@mail.umsl.edu>
2017-11-14 10:25:56 -0800

    Snacky imported and readme added

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule} from "@angular/material";
+import {MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule} from "@angular/material";
 
 @NgModule({
   imports: [
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule
   ],
   declarations: []
 })
